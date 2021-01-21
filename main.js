f69fd417f2602d3863f53906da12b5099c7d5b30
y715 <77558787+y715@users.noreply.github.com>
2021-01-21 10:22:21 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+/* ハンバーガーメニュー */
+$(function(){
+	$('.nav_toggle').on('click', function () {
+		$('.nav_toggle, .nav').toggleClass('show');
+	});
+	})
+
+/* スクロール */
+$('a[href^="#"]').click(function() {
+	// スクロールの速度
+	var speed = 400; // ミリ秒で記述
+	var href = $(this).attr("href");
+	var target = $(href == "#" || href == "" ? 'html' : href);
+	var position = target.offset().top;
+	$('body,html').animate({
+		scrollTop: position
+	}, speed, 'swing');
+	return false;
+	});
